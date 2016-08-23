141b21182dda560924dcc312eb48d91c4acf8fa1
1994skumar <1994skumar@gmail.com>
2016-08-23 18:22:08 +0530

    testing

---
+++ server3.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app=express();
+var path = require('path');
+var http = require('http');
+
+app.get("/",function(req,res){
+res.send("hello");
+})
+
+
+var server   = http.createServer(app);
+server.listen(1338, function() {
+  console.log("Node server running on http://localhost:1330");
+});
+
+module.exports = app;
